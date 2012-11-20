9b8ae0a1c3db4b95c793a0e279174f2c5c7f45ce
Alexandre Bulté <alexandre@bulte.net>
2012-11-20 12:19:58 +0100

    Post commit hook

---
+++ post-commit.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# regenerate site via jekyll
+/usr/bin/jekyll
+# git push (this branch - source)
+git push origin source
+# get last commit message
+COMMIT_MSG=`git log -n 1 HEAD --format=format:%s%n%b`
+# clean static export
+rm -rf ../abulte.github.com/*
+# copy new static export
+cp -a _site/* ../abulte.github.com/
+# cd static export
+cd ../abulte.github.com
+# git commit (master branch)
+git commit -a -m "$COMMIT_MSG"
+# git push (master branch)
+git push origin master
